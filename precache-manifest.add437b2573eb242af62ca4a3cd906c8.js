797c085ce74e62a2aba30d4e2886a0b4056d4107
Priyank tyagi <priyank.tyagi@iskpro.com>
2020-05-10 20:50:38 +0530

    Updates

---
+++ precache-manifest.add437b2573eb242af62ca4a3cd906c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ac5fdb878426a82a0189ca40713e8a",
+    "url": "/Priyank-Tyagi/covid/index.html"
+  },
+  {
+    "revision": "9319230c239e4e1b12a1",
+    "url": "/Priyank-Tyagi/covid/static/css/main.80c6246e.chunk.css"
+  },
+  {
+    "revision": "ff26cd1baf267936e528",
+    "url": "/Priyank-Tyagi/covid/static/js/2.ed465d03.chunk.js"
+  },
+  {
+    "revision": "cbba99a0773ac0cb926d071cbb35fb79",
+    "url": "/Priyank-Tyagi/covid/static/js/2.ed465d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9319230c239e4e1b12a1",
+    "url": "/Priyank-Tyagi/covid/static/js/main.5efb9030.chunk.js"
+  },
+  {
+    "revision": "200c5361bd508610be9c",
+    "url": "/Priyank-Tyagi/covid/static/js/runtime-main.a3d677e9.js"
+  },
+  {
+    "revision": "7971da3c65cb56b6852e9e4fe85a261b",
+    "url": "/Priyank-Tyagi/covid/static/media/banner.7971da3c.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Priyank-Tyagi/covid/precache-manifest.add437b2573eb242af62ca4a3cd906c8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Priyank-Tyagi/covid/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
