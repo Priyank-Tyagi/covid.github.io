54da8387e595fbb3fad6ca62f5722fcd16f5545a
Priyank tyagi <priyank.tyagi@iskpro.com>
2020-05-10 21:30:58 +0530

    h

---
+++ build/precache-manifest.08750fcb971e62d7b0f07fe5cae18597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10c7ba8e33ae7370b8fc6034e3c05de",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "9319230c239e4e1b12a1",
+    "url": "/covid/static/css/main.80c6246e.chunk.css"
+  },
+  {
+    "revision": "add7c58103027931975f",
+    "url": "/covid/static/js/2.b42812b7.chunk.js"
+  },
+  {
+    "revision": "cbba99a0773ac0cb926d071cbb35fb79",
+    "url": "/covid/static/js/2.b42812b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9319230c239e4e1b12a1",
+    "url": "/covid/static/js/main.5efb9030.chunk.js"
+  },
+  {
+    "revision": "2efb2bde5ae38bc0020d",
+    "url": "/covid/static/js/runtime-main.d61a144f.js"
+  },
+  {
+    "revision": "7971da3c65cb56b6852e9e4fe85a261b",
+    "url": "/covid/static/media/banner.7971da3c.png"
+  }
+]);
